d6ce0947d8543d01bb29b6b530fc22a00f51c486
Marcin Lewandowski <marcin@radiokit.org>
2016-11-05 01:01:44 +0100

    Use new base modules

--- lib/membrane_element_opus/decoder.ex
+++ lib/membrane_element_opus/decoder.ex
@@ -1,2 +1,8 @@
 defmodule Membrane.Element.Opus.Decoder do
+  use Membrane.Element.Base.Filter
+
+
+  def handle_buffer(%Membrane.Caps{content: "audio/x-opus"}, data, state) do
+    {:ok, state} # TODO
+  end
 end
--- lib/membrane_element_opus/encoder.ex
+++ lib/membrane_element_opus/encoder.ex
@@ -1,2 +1,8 @@
 defmodule Membrane.Element.Opus.Encoder do
+  use Membrane.Element.Base.Filter
+
+
+  def handle_buffer(%Membrane.Caps{content: "audio/x-raw"}, data, state) do
+    {:ok, state} # TODO
+  end
 end
