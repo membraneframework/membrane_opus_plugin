599f6c5344263ec8ea1f1d4e1d697d0a391234b1
Marcin Lewandowski <marcin@radiokit.org>
2016-11-09 00:07:10 +0100

    Improved docs

--- lib/membrane_element_opus/encoder.ex
+++ lib/membrane_element_opus/encoder.ex
@@ -1,14 +1,20 @@
 defmodule Membrane.Element.Opus.EncoderOptions do
   defstruct \
     bitrate:     96 * 1024,
-    sample_rate: 48000, # FIXME only 48kHz is supported at the moment
-    channels:    2,     # FIXME only stereo is supported at the moment
+    sample_rate: 48000, # TODO only 48kHz is supported at the moment
+    channels:    2,     # TODO only stereo is supported at the moment
     application: :audio,
-    frame_size:  10
+    frame_size:  10     # one of 2.5, 5, 10, 20, 40, 60
 end
 
 
 defmodule Membrane.Element.Opus.Encoder do
+  @moduledoc """
+  This element performs encoding of raw audio using Opus codec.
+
+  At the moment it accepts only 48000 kHz, stereo, 16-bit, little-endian audio.
+  """
+
   use Membrane.Element.Base.Filter
 
 
@@ -17,14 +23,17 @@ defmodule Membrane.Element.Opus.Encoder do
       {:ok, native} ->
         case Membrane.Element.Opus.EncoderNative.set_bitrate(native, bitrate) do
           :ok ->
-            # Cache size in samples and bytes of one packet for given Opus
-            # frame size.
+            # Store size in samples and bytes of one packet for given Opus
+            # frame size. This is later required both by encoder (it expects
+            # samples' count to each encode call) and by algorithm chopping
+            # incoming buffers into packets of size expected by the encoder.
             #
-            # Byte size is equal to amount of samples for duration specified by
-            # frame size for given sample rate multiplied by amount of channels
-            # multiplied by 2 (Opus always # uses 16-bit frames).
-            # FIXME Hardcoded 2 channels, 48 kHz
-            packet_size_in_samples = compute_samples_for_frame_size(sample_rate, frame_size);
+            # Packet size in bytes is equal to amount of samples for duration
+            # specified by frame size for given sample rate multiplied by amount
+            # of channels multiplied by 2 (Opus always uses 16-bit frames).
+            #
+            # TODO Hardcoded 2 channels, 48 kHz
+            packet_size_in_samples = packet_samples_count(sample_rate, frame_size);
             packet_size_in_bytes = packet_size_in_samples * 2 * 2;
 
             {:ok, %{
@@ -45,6 +54,7 @@ defmodule Membrane.Element.Opus.Encoder do
   end
 
 
+  # TODO support other sample rates and channels
   def handle_buffer(%Membrane.Caps{content: "audio/x-raw", sample_rate: 48000, channels: 2, endianness: :le, frame_size: 2}, data, %{packet_size_in_samples: packet_size_in_samples, packet_size_in_bytes: packet_size_in_bytes, native: native, queue: queue} = state) do
     # If we have more data in the buffer than required, split them as packets
     # of required size recursively. Keep the remaining buffer for future calls.
@@ -54,31 +64,41 @@ defmodule Membrane.Element.Opus.Encoder do
   end
 
 
-  # For 48kHz
-  defp compute_samples_for_frame_size(48000, 60), do: 2880
-  defp compute_samples_for_frame_size(48000, 40), do: 1820
-  defp compute_samples_for_frame_size(48000, 20), do: 960
-  defp compute_samples_for_frame_size(48000, 10), do: 480
-  defp compute_samples_for_frame_size(48000, 5), do: 240
-  defp compute_samples_for_frame_size(48000, 2.5), do: 120
+  # TODO only For 48kHz
+  defp packet_samples_count(48000, 60), do: 2880
+  defp packet_samples_count(48000, 40), do: 1820
+  defp packet_samples_count(48000, 20), do: 960
+  defp packet_samples_count(48000, 10), do: 480
+  defp packet_samples_count(48000, 5), do: 240
+  defp packet_samples_count(48000, 2.5), do: 120
 
 
-  defp prepare_encoded_buffers(data, packet_size_in_bytes, packet_size_in_samples, native, acc) do
+  # Chops queue with data into packets of size expected by the Opus encoder.
+  # It encodes each packet and creates new buffer to be sent to linked elements.
+  # At the end of recursion it returns `{list_of_encoded_buffers, remaining_queue}`.
+  defp prepare_encoded_buffers(queue, packet_size_in_bytes, packet_size_in_samples, native, acc) do
     cond do
-      byte_size(data) > packet_size_in_bytes ->
-        << packet_data :: binary-size(packet_size_in_bytes), rest :: binary >> = data
-        prepare_encoded_buffers(rest, packet_size_in_bytes, packet_size_in_samples, native, [encode(packet_data, native, packet_size_in_samples)|acc])
+      # We have more queue in the queue than we need for single packet.
+      # Encode one packet and recurse.
+      byte_size(queue) > packet_size_in_bytes ->
+        << packet_data :: binary-size(packet_size_in_bytes), rest :: binary >> = queue
+        prepare_encoded_buffers(rest, packet_size_in_bytes, packet_size_in_samples, native, [encode(native, packet_data, packet_size_in_samples)|acc])
 
-      byte_size(data) == packet_size_in_bytes ->
-        {[encode(data, native, packet_size_in_samples)|acc], << >>}
+      # We have exact amount of queue in the queue for a single packet.
+      # Encode it and return with empty queue.
+      byte_size(queue) == packet_size_in_bytes ->
+        {[encode(native, queue, packet_size_in_samples)|acc], << >>}
 
-      byte_size(data) < packet_size_in_bytes ->
-        {acc, data}
+      # We have less queue in the queue than we need for a single packet.
+      # Do nothing and return with unmodified queue.
+      byte_size(queue) < packet_size_in_bytes ->
+        {acc, queue}
     end
   end
 
 
-  defp encode(packet_data, native, packet_size_in_samples) do
+  # Does the actual encoding of packet data that already has desired size.
+  defp encode(native, packet_data, packet_size_in_samples) do
     {:ok, encoded_data} = Membrane.Element.Opus.EncoderNative.encode_int(native, packet_data, packet_size_in_samples)
 
     {%Membrane.Caps{content: "audio/x-opus"}, encoded_data}
