33700c572c8a1127974d7385688254044db44d95
Mateusz Front <mateusz.front@swmansion.com>
2020-12-02 15:57:02 +0100

    bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Opus.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @github_url "https://github.com/membraneframework/membrane_opus_plugin"
 
   def project do
