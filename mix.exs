63d3b3551d34403c5eded4787a6e8da1b6aa0f6c
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-03-25 07:06:40 +0100

    Bump version to 0.12.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Opus.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.11.0"
+  @version "0.12.0"
   @github_url "https://github.com/membraneframework/membrane_opus_plugin"
 
   def project do
