6e44a1d2bea69832972c2f02520e421f32b49223
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2022-09-02 14:07:27 +0200

    Update links in mix.exs

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Membrane.Opus.Plugin.Mixfile do
       # docs
       name: "Membrane Opus plugin",
       source_url: @github_url,
-      homepage_url: "https://membraneframework.org",
+      homepage_url: "https://membrane.stream",
       docs: docs()
     ]
   end
@@ -68,7 +68,7 @@ defmodule Membrane.Opus.Plugin.Mixfile do
       licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => @github_url,
-        "Membrane Framework Homepage" => "https://membraneframework.org"
+        "Membrane Framework Homepage" => "https://membrane.stream"
       },
       files: ["lib", "mix.exs", "README*", "LICENSE*", ".formatter.exs", "bundlex.exs", "c_src"],
       exclude_patterns: [~r"c_src/.*/_generated.*"]
