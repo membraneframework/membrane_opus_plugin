ca9bc9160e104ab869ef992449dd42c80667e92e
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2023-06-27 16:16:50 +0200

    Release v0.17.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Opus.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.17.0"
+  @version "0.17.1"
   @github_url "https://github.com/membraneframework/membrane_opus_plugin"
 
   def project do
@@ -36,9 +36,9 @@ defmodule Membrane.Opus.Plugin.Mixfile do
   defp deps do
     [
       {:bunch, "~> 1.3"},
-      {:membrane_core, "~> 0.12.0"},
+      {:membrane_core, "~> 0.12.3"},
       {:membrane_opus_format, "~> 0.3.0"},
-      {:membrane_raw_audio_format, "~> 0.10.0"},
+      {:membrane_raw_audio_format, "~> 0.11.0"},
       {:unifex, "~> 1.0"},
       {:membrane_common_c, "~> 0.15.0"},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
