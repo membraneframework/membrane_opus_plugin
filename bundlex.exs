0d32083b5afedae1239aef4c31eb745e7add06bd
Noarkhh <jakub.pryc@gmail.com>
2023-12-14 09:11:49 +0100

    Start using Precompiled Dependency Provider

--- bundlex.exs
+++ bundlex.exs
@@ -7,37 +7,28 @@ defmodule Membrane.Opus.BundlexProject do
     ]
   end
 
-  defp get_opus_url() do
-    url_prefix =
-      "https://github.com/membraneframework-precompiled/precompiled_opus/releases/latest/download/opus"
-
-    case Bundlex.get_target() do
-      %{os: "linux"} ->
-        {:precompiled, "#{url_prefix}_linux.tar.gz"}
-
-      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-        {:precompiled, "#{url_prefix}_macos_intel.tar.gz"}
-
-      %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
-        {:precompiled, "#{url_prefix}_macos_arm.tar.gz"}
-
-      _other ->
-        nil
-    end
-  end
-
   def natives(_platform) do
     [
       decoder: [
         sources: ["decoder.c"],
-        os_deps: [{[get_opus_url(), :pkg_config], "opus"}],
+        os_deps: [
+          opus: [
+            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:opus)},
+            :pkg_config
+          ]
+        ],
         interface: :nif,
         preprocessor: Unifex
       ],
       encoder: [
         deps: [membrane_common_c: :membrane],
         sources: ["encoder.c"],
-        os_deps: [{[get_opus_url(), :pkg_config], "opus"}],
+        os_deps: [
+          opus: [
+            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:opus)},
+            :pkg_config
+          ]
+        ],
         interface: :nif,
         preprocessor: Unifex
       ]
