2c793dce91db25e509eb58465d1aa251cceef3a4
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-04-20 14:06:24 +0200

    Update mix.lock

--- mix.exs
+++ mix.exs
@@ -44,9 +44,9 @@ defmodule Membrane.Opus.Plugin.Mixfile do
       {:membrane_common_c, "~> 0.16.0"},
       {:bundlex, "~> 1.2"},
       {:membrane_precompiled_dependency_provider, "~> 0.2.0"},
-      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
-      {:credo, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: :dev, runtime: false},
       {:membrane_file_plugin, "~> 0.16.0", only: :test},
       {:membrane_raw_audio_parser_plugin, "~> 0.4.0", only: :test}
     ]
@@ -54,11 +54,13 @@ defmodule Membrane.Opus.Plugin.Mixfile do
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling]
+      flags: [:error_handling],
+      plt_add_apps: [:syntax_tools]
     ]
 
     if System.get_env("CI") == "true" do
       # Store PLTs in cacheable directory for CI
+      File.mkdir_p!(Path.join([__DIR__, "priv", "plts"]))
       [plt_local_path: "priv/plts", plt_core_path: "priv/plts"] ++ opts
     else
       opts
