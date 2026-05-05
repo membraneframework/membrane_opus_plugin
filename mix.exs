620ccf08ce4e28a5d8be237a9800ab30ed690906
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-05 14:20:51 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Opus.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.20.6"
+  @version "0.20.7"
   @github_url "https://github.com/membraneframework/membrane_opus_plugin"
 
   def project do
